9be6cef55f7e2b86facb821a11199b5d94cea05c
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-12-22 13:14:31 -0600

    chore: add metadata to pyproject.toml

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,18 +1,12 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import datetime
-
 from singer_sdk.testing import get_standard_tap_tests
 
 from tap_pocket.tap import TapPocket
 
-SAMPLE_CONFIG = {
-    "consumer_key": "100055-b43370578682acfb76b20b5",
-    "access_token": "dcdd762a-a84e-d90c-bcfc-5380e0",
-}
+SAMPLE_CONFIG = {}
 
 
-# Run standard built-in tap tests from the SDK:
 def test_standard_tap_tests():
     """Run standard tap tests from the SDK."""
     tests = get_standard_tap_tests(TapPocket, config=SAMPLE_CONFIG)
