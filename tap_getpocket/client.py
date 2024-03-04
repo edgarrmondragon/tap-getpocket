ba7fa9a7104385c3fe871419022b16f32f18395e
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-03-04 14:57:24 -0600

    refactor: Remove redundant paginator implementation

--- tap_getpocket/client.py
+++ tap_getpocket/client.py
@@ -2,30 +2,8 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from singer_sdk.pagination import BaseOffsetPaginator
 from singer_sdk.streams import RESTStream
-
-if TYPE_CHECKING:
-    from requests import Response
-
-
-class PocketPaginator(BaseOffsetPaginator):
-    """Pocket API pagination handler."""
-
-    def has_more(self, response: Response) -> bool:
-        """Return True if there are more pages to fetch.
-
-        Args:
-            response: The most recent response object.
-
-        Returns:
-            Whether there are more pages to fetch.
-        """
-        if response.json()["list"]:
-            return True
-        return False
 
 
 class PocketStream(RESTStream[int]):
@@ -35,13 +13,13 @@ class PocketStream(RESTStream[int]):
     records_jsonpath = "$.list.*"
     page_size = 100
 
-    def get_new_paginator(self) -> PocketPaginator:
+    def get_new_paginator(self) -> BaseOffsetPaginator:
         """Return a new instance of the paginator for this stream.
 
         Returns:
             A new instance of the paginator for this stream.
         """
-        return PocketPaginator(0, self.page_size)
+        return BaseOffsetPaginator(0, self.page_size)
 
     @property
     def http_headers(self) -> dict[str, str]:
